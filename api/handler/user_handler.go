a83027b6b2aa3d5214114b273e860d1da08b6cda
hoffax <douglashoffmann@outlook.es>
2023-06-11 00:33:28 -0400

    removed everything from old version

--- api/handler/user_handler.go
+++
@@ -1,9 +0,0 @@
-package handler
-
-import (
-	"github.com/hoffax/prodapi/service"
-)
-
-type UserHandler struct {
-	UserService *service.UserService
-}
--- repository/session/session_repository.go
+++
@@ -1,8 +0,0 @@
-package session
-
-import "github.com/hoffax/prodapi/dbtypes"
-
-type Repository interface {
-	// You can add DB here
-	GetByID(userID string) (*dbtypes.User, error)
-}
